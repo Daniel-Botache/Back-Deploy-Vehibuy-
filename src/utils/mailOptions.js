53c69d5906242dc93da715af2866d2aed2a9495d
Daniel Botache <120441453+Daniel-Botache@users.noreply.github.com>
2023-09-20 01:28:16 -0500

    Update mailOptions.js

--- src/utils/mailOptions.js
+++ src/utils/mailOptions.js
@@ -7,27 +7,27 @@ let mailOptions = (to, subject, html) => {
     attachments: [
       {
         filename: "facebook.png",
-        path: "/opt/render/project/src/ultils/icons/facebook.png",
+        path: "/opt/render/project/src/utils/icons/facebook.png",
         cid: "fb",
       },
       {
         filename: "LOGO.png",
-        path: "/opt/render/project/src/ultils/icons/LOGO.png",
+        path: "/opt/render/project/src/utils/icons/LOGO.png",
         cid: "logo",
       },
       {
         filename: "linkedin.png",
-        path: "/opt/render/project/src/ultils/icons/linkedin.png",
+        path: "/opt/render/project/src/utils/icons/linkedin.png",
         cid: "ld",
       },
       {
         filename: "gmail.png",
-        path: "/opt/render/project/src/ultils/icons/gmail.png",
+        path: "/opt/render/project/src/utils/icons/gmail.png",
         cid: "gmail",
       },
       {
         filename: "wapp.png",
-        path: "/opt/render/project/src/ultils/icons/wapp.png",
+        path: "/opt/render/project/src/utils/icons/wapp.png",
         cid: "wapp",
       },
     ],
