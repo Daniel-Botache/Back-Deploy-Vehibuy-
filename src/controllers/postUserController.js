0cab99a99907470ba478e7e4b863a558104dc213
Daniel-Botache <daniel.botache@gmail.com>
2023-09-21 13:07:56 -0500

    secambia nodemailer

--- src/controllers/postUserController.js
+++ src/controllers/postUserController.js
@@ -46,7 +46,7 @@ const postUserController = async (dataUserBody) => {
     });
 
     const subject = "Account Verification on Vehibuy.com";
-    let link = `https://vehibuy-rho.vercel.app/user/verify/${token}`;
+    let link = `https://back-vehibuy.onrender.com/user/verify/${token}`;
     const html = verifyHTML(link);
     transporter.sendMail(mailOptions(email, subject, html), (error, info) => {
       if (error) {
